4edd3019a1212338b3e7ea94d65128417fedd12e
Ashutosh Kamboj <57835919+ashukam-bro@users.noreply.github.com>
2020-08-01 14:58:11 +0530

    Add files via upload

---
+++ word.py
@@ -0,0 +1,3 @@
+word_list = "apple mango banana jackfruit lemon orange pineapple melon papaya cherry peach litchi grapes berries tomato watermelon cheeku lime blueberry strawberry coconut" \
+            " pear apricot avocado blackberry grapefruit kiwi plum raspberry pomegranete fig tangerine "
+word_list=word_list.split(" ")
